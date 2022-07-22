5e8f2104d8fda1011aa1f051e1d75776582dcc82
Cody Olsen <stipsan@gmail.com>
2022-07-22 06:30:30 +0200

    prep

--- pages/[[...index]].tsx
+++ pages/[[...index]].tsx
@@ -1,6 +1,34 @@
-import config from '../sanity.config'
+import _config from '../sanity.config'
 import {Studio} from 'sanity'
+import {useState} from 'react'
+import Head from 'next/head'
 
 export default function IndexPage() {
-  return <Studio config={config} />
+  const [config, setConfig] = useState(_config)
+
+  return (
+    <>
+      <Head>
+        {/* Studio implements display cutouts CSS (The iPhone Notch ™ ) and needs `viewport-fit=covered` for it to work correctly */}
+        <meta
+          name="viewport"
+          content="width=device-width, viewport-fit=cover"
+        />
+        {/* These theme-color tags makes the Studio look really really good on devices like iPads as the browser chrome adopts the Studio background */}
+        <meta
+          key="theme-color-light"
+          name="theme-color"
+          content={config.theme.color.light.default.base.bg}
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          key="theme-color-dark"
+          name="theme-color"
+          content={config.theme.color.dark.default.base.bg}
+          media="(prefers-color-scheme: dark)"
+        />
+      </Head>
+      <Studio config={config} />
+    </>
+  )
 }
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,12 @@
-import {createConfig} from 'sanity'
+import {createConfig, defaultTheme} from 'sanity'
 import {deskTool} from 'sanity/desk'
 
 import {schemaTypes} from './schemas'
 
 export default createConfig({
+  // allows reading the default theme variables while the custom theme is loading
+  theme: defaultTheme,
+
   name: 'themer-next-runtime',
   title: 'My Sanity Project',
   projectId: 'kieaexhf',
